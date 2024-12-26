917948f442bd93745f9a45645d77016154d9edcd
Nima Rezaee <nyma0458@gmail.com>
2024-12-26 07:44:12 +0330

    commit33

--- Project/Project.Mvc/obj/Debug/net8.0/Project.Presentation.AssemblyInfo.cs
+++ Project/Project.Mvc/obj/Debug/net8.0/Project.Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81aa3deaae63658c24f45406065fcb5f77ba8500")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+546fcee27a75483f549aade2a38a939750fdeec4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
